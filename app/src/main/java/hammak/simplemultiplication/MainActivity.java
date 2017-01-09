dd4248250528a975d66ff329fab6480090e7fe8a
Saint Jakuzi <saint3543jakuzi@gmail.com>
2017-01-09 15:06:06 +0200

    Save counter function (Save #2)

--- app/src/main/java/hammak/simplemultiplication/MainActivity.java
+++ app/src/main/java/hammak/simplemultiplication/MainActivity.java
@@ -2,6 +2,7 @@ package hammak.simplemultiplication;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,13 +14,17 @@ import android.widget.TextView;
 import java.util.Locale;
 import java.util.Objects;
 
+import static android.content.SharedPreferences.*;
+
 public class MainActivity extends AppCompatActivity implements android.view.View.OnClickListener {
 
     private TextView tvTaskL, tvTaskR, tvCounter, tvUserAnswer, tvRightAnswer;
     private EditText etAnswer;
 
-    // Task numbers
-    private int fNum, sNum, counter = 0;
+    private SharedPreferences pref;
+    private static final String COUNTER_SAVED_STATE_NAME = "counter";
+
+    private int fNum, sNum, counter;
 
     @SuppressLint("StaticFieldLeak")
     private static Context context;
@@ -46,44 +51,26 @@ public class MainActivity extends AppCompatActivity implements android.view.View
         // OnClickListeners
         bAnswer.setOnClickListener(this);
 
+        pref = getPreferences(MODE_PRIVATE);
+        counter = pref.getInt(COUNTER_SAVED_STATE_NAME, 0);
+
         updateCounter(counter);
         setNewTask();
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        // Saves counter state
+        pref = getPreferences(MODE_PRIVATE);
+        Editor ed = pref.edit();
+        ed.putInt(COUNTER_SAVED_STATE_NAME, counter);
+        ed.apply();
+    }
+
     private void updateCounter(int counter) {
         tvCounter.setText(String.format(Locale.ENGLISH, "%d", counter));
-        if (counter == 0)
-            tvCounter.setBackgroundColor(ContextCompat.getColor(context, R.color.counter0));
-        else if (counter == 5)
-            tvCounter.setBackgroundColor(ContextCompat.getColor(context, R.color.counter5));
-        else if (counter == 10)
-            tvCounter.setBackgroundColor(ContextCompat.getColor(context, R.color.counter10));
-        else if (counter == 12)
-            tvCounter.setBackgroundColor(ContextCompat.getColor(context, R.color.counter12));
-        else if (counter == 13)
-            tvCounter.setBackgroundColor(ContextCompat.getColor(context, R.color.counter13));
-        else if (counter == 20)
-            tvCounter.setBackgroundColor(ContextCompat.getColor(context, R.color.counter20));
-        else if (counter == 33)
-            tvCounter.setBackgroundColor(ContextCompat.getColor(context, R.color.counter33));
-        else if (counter == 50)
-            tvCounter.setBackgroundColor(ContextCompat.getColor(context, R.color.counter50));
-        else if (counter == 75)
-            tvCounter.setBackgroundColor(ContextCompat.getColor(context, R.color.counter75));
-        else if (counter == 79)
-            tvCounter.setBackgroundColor(ContextCompat.getColor(context, R.color.counter79));
-        else if (counter == 100)
-            tvCounter.setBackgroundColor(ContextCompat.getColor(context, R.color.counter100));
-        else if (counter == 255)
-            tvCounter.setBackgroundColor(ContextCompat.getColor(context, R.color.counter255));
-        else if (counter == 500)
-            tvCounter.setBackgroundColor(ContextCompat.getColor(context, R.color.counter500));
-        else if (counter == 666)
-            tvCounter.setBackgroundColor(ContextCompat.getColor(context, R.color.counter666));
-        else if (counter == 1618)
-            tvCounter.setBackgroundColor(ContextCompat.getColor(context, R.color.counter1618));
-        else if (counter == 2147483647)
-            tvCounter.setBackgroundColor(ContextCompat.getColor(context, R.color.counter2147483647));
     }
 
     private void setNewTask(){
